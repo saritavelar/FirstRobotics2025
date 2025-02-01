ddc4526045b2f8b7edf13a56f771bcbf909f9891
saritavelar <160293737+saritavelar@users.noreply.github.com>
2025-02-01 11:02:58 -0800

    bleh

--- Ropotgodrive.py
+++ Ropotgodrive.py
@@ -1,12 +1,8 @@
 #import statements
-import __main__
 import rev
 import wpilib
 import wpilib.drive
-#import constants
-
-
-from wpilib import RobotDrive
+from constants import constants
 from wpilib.drive import MeccanumDrive
 from rev import CANSparkMax, MotorType
 
@@ -41,33 +37,37 @@ class MyRobot(wpilib.TimedRobot):
         self.left4 = rev.CANSparkMax(4,kLeftMotor2Port, rev.MotorType.kBrushed)
         self.shooterMotor = rev.CANSparkMax(shooterMotorPort, rev.MotorType.kBrushed)
 
+        # Set right-side motors to be inverted
+        self.right1.setInverted(True)
+        self.right3.setInverted(True)
+
 #Make the MeccanumDrive work
-        self.drive = MeccanumDrive(self.left1, self.left2, self.right1, self.right2)
+        self.drive = MeccanumDrive(self.left2, self.left4, self.right1, self.right3)
 #Make the Xbox controller work
         self.controller = wpilib.XboxController(0)
-        self.drive = MeccanumDrive(self.left1, self.left2, self.right1, self.right2)
-        '''''
+        self.drive = MeccanumDrive(self.left2, self.left4, self.right1, self.right3)
+        
         #Limit the speed of the motors (I hope this is what this means lol)
         self.right1.setSmartCurrentLimit(constants.kDriveCurrentLimit)
         self.right2.setSmartCurrentLimit(constants.kDriveCurrentLimit)
         self.left1.setSmartCurrentLimit(constants.kDriveCurrentLimit)
-        self.left2.setSmartCurrentLimit(constants.kDriveCurrentLimit)'''
-        
+        self.left2.setSmartCurrentLimit(constants.kDriveCurrentLimit)
 
-def teleopPeriodic(self):
-#This part of the code runs repeatedly during teleop mode and where the robot "uses" the knowlegde you gave it during the Intialize part
-            self.drive.driveCartesian(self.stick.getX(), self.stick.getY(), self.stick.getZ())
+    def teleopPeriodic(self):
+        # This part of the code runs repeatedly during teleop mode and where the robot "uses" the knowledge you gave it during the Initialize part
+        self.drive.driveCartesian(self.controller.getX(), self.controller.getY(), self.controller.getZ())
 
-#okay so TBH the following code is what chatgpt told me to do to change how the controller controls the drive
-            left_y = self.controller.getLeftY()  # Left joystick Y-axis (forward/backward)
-            right_y = self.controller.getRightY()  # Right joystick Y-axis (forward/backward)
-            rotation = self.controller.getRightX()  # Right joystick X-axis (turning)
-            self.robot_drive.driveCartesian(left_y, right_y, rotation)
+        # okay so TBH the following code is what chatgpt told me to do to change how the controller controls the drive
+        left_y = self.controller.getLeftY()  # Left joystick Y-axis (forward/backward)
+        right_y = self.controller.getRightY()  # Right joystick Y-axis (forward/backward)
+        rotation = self.controller.getRightX()  # Right joystick X-axis (turning)
+        self.drive.driveCartesian(left_y, right_y, rotation)
 
-            if self.driverController.getRightTriggerAxis() > 0.2:
-             self.shooterMotor.set(0.5) #1 is full speed so 0.5 is half speed go higher number if it doesnt work.
-            else:
-                self.shooterMotor.set(0)
+        if self.driverController.getRightTriggerAxis() > 0.2:
+            self.shooterMotor.set(0.5)  # 1 is full speed so 0.5 is half speed go higher number if it doesn't work.
+        else:
+            self.shooterMotor.set(0)
+
 if __name__ == "__main__":
     wpilib.run(MyRobot)
 
