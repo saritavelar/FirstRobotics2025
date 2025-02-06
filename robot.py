7f0f69d07eec4c9f592770ede5f3fca88b7f971e
josif9 <160293929+josif9@users.noreply.github.com>
2025-02-05 17:01:56 -0800

    Add files via upload

---
+++ robot.py
@@ -0,0 +1,75 @@
+#import statements
+import rev
+import wpilib
+import wpilib.drive
+#from constants import constants
+from wpilib.drive import MecanumDrive
+from rev import SparkMax, SparkLowLevel
+
+#Declare the class I'm sure why but you have to do this
+class MyRobot(wpilib.TimedRobot):
+
+
+#this part of the code is for autonomous mode which we havent started yet so the "pass" tells the robot to ignore it for now
+    def AutonomousInit(self):
+          pass
+    def AutonomousPeriodic(self):
+         pass
+    
+
+#Initialize ur robot this parts makes everything work you set the robot up with al the "knowledge"
+    def robotInit(self):
+        kRightMotor1Port = 1
+        kRightMotor2Port = 3
+        kLeftMotor1Port = 2
+        kLeftMotor2Port = 4
+        shooterMotorPort = 5
+
+
+
+
+#Make the motors work using SparkMax
+        self.right1 = rev.SparkMax(kRightMotor1Port, SparkLowLevel.MotorType.kBrushed)
+        self.right3 = rev.SparkMax(kRightMotor2Port, SparkLowLevel.MotorType.kBrushed)
+
+
+        self.left2 = rev.SparkMax(kLeftMotor1Port, SparkLowLevel.MotorType.kBrushed)
+        self.left4 = rev.SparkMax(kLeftMotor2Port, SparkLowLevel.MotorType.kBrushed)
+        self.shooterMotor = rev.SparkMax(shooterMotorPort, SparkLowLevel.MotorType.kBrushed)
+
+        # Set right-side motors to be inverted
+        self.right1.setInverted(True)
+        self.right3.setInverted(True)
+
+#Make the MeccanumDrive work
+        self.drive = MecanumDrive(self.left2, self.left4, self.right1, self.right3)
+#Make the Xbox controller work
+        self.controller = wpilib.XboxController(0)
+        self.drive = MecanumDrive(self.left2, self.left4, self.right1, self.right3)
+        
+        #Limit the speed of the motors (I hope this is what this means lol)
+        '''
+        self.right1.setSmartCurrentLimit(constants.kDriveCurrentLimit)
+        self.right2.setSmartCurrentLimit(constants.kDriveCurrentLimit)
+        self.left1.setSmartCurrentLimit(constants.kDriveCurrentLimit)
+        self.left2.setSmartCurrentLimit(constants.kDriveCurrentLimit)
+        '''
+    def teleopPeriodic(self):
+        # This part of the code runs repeatedly during teleop mode and where the robot "uses" the knowledge you gave it during the Initialize part
+        #self.drive.driveCartesian(self.controller.getX(), self.controller.getY(), self.controller.getZ())
+
+        # okay so TBH the following code is what chatgpt told me to do to change how the controller controls the drive
+        left_y = self.controller.getLeftY()  # Left joystick Y-axis (forward/backward)
+        right_y = self.controller.getRightY()  # Right joystick Y-axis (forward/backward)
+        rotation = self.controller.getRightX()  # Right joystick X-axis (turning)
+        self.drive.driveCartesian(left_y, right_y, rotation)
+
+        if self.controller.getRightTriggerAxis() > 0.2:
+            self.shooterMotor.set(0.5)  # 1 is full speed so 0.5 is half speed go higher number if it doesn't work.
+        else:
+            self.shooterMotor.set(0)
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
+
+ 
