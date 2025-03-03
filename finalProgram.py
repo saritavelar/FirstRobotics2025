78d04a887df6715461ebf42f35e3fc871e958974
josif9 <160293929+josif9@users.noreply.github.com>
2025-03-02 20:46:45 -0800

    Update finalProgram.py

--- finalProgram.py
+++ finalProgram.py
@@ -1,7 +1,4 @@
-# TODO: insert robot code here
-
 # Import statements
-
 import wpilib
 import math 
 import wpilib.drive
@@ -15,12 +12,14 @@ LEFT_FRONT_MOTOR = 2
 LEFT_REAR_MOTOR = 3
 RIGHT_REAR_MOTOR = 5
 INTAKE_MOTOR = 4
-#ELEVATOR_MOTOR = 6
 
+# Initializes both USB cameras on the robot
 CameraServer.startAutomaticCapture(0)
 CameraServer.startAutomaticCapture(1)
 
-#Declare the class I'm sure why but you have to do this
+# Declaring the class so that the driver station is able
+# to identify that this is the code that the roboRIO 
+# will use to allow the robot to work properly.
 class MyRobot(wpilib.TimedRobot):
     
     # Allows the motor variables to be called on anywhere in the program
@@ -29,21 +28,15 @@ class MyRobot(wpilib.TimedRobot):
     #Initialize ur robot this parts makes everything work you set the robot up with al the "knowledge"
     def robotInit(self):
         
-        #initializes camera(s)
-        
-        #CameraServer.startAutomaticCapture(0)
-        #CameraServer.startAutomaticCapture(1)
-        
-        # Make the motors work using SparkMax 
-        # right side motors
+        # Stating each motor controler and the motor it is connected to.
+        #right side motors
         self.rightFront = SparkMax(RIGHT_FRONT_MOTOR, SparkLowLevel.MotorType.kBrushed)
         self.rightRear = SparkMax(RIGHT_REAR_MOTOR, SparkLowLevel.MotorType.kBrushed)
-        # left side motors
+        #left side motors
         self.leftFront = SparkMax(LEFT_FRONT_MOTOR, SparkLowLevel.MotorType.kBrushed)
         self.leftRear = SparkMax(LEFT_REAR_MOTOR, SparkLowLevel.MotorType.kBrushed)
-        # intake and elevator motors
+        #intake and elevator motors
         self.intake = SparkMax(INTAKE_MOTOR, SparkLowLevel.MotorType.kBrushed)
-        #self.elevator = SparkMax(ELEVATOR_MOTOR, SparkLowLevel.MotorType.kBrushed)
         
         # Creates mecanum drive 
         self.drive = MecanumDrive(self.leftFront, self.leftRear, self.rightFront, self.rightRear)
@@ -51,37 +44,40 @@ class MyRobot(wpilib.TimedRobot):
         self.controller = wpilib.XboxController(0)
 
 
-    #this part of the code is for autonomous mode which we havent started yet so the "pass" tells the robot to ignore it for now
+# Here, a timer is created so that the autonomous periodic function can run conditionals based on time intervals.
     def autonomousInit(self):
         self.drive.driveCartesian(0,0,0)
         self.intake.set(0)
         self.timer = wpilib.Timer()
         self.timer.reset()
         self.timer.start()
-        self.intakeOn = 0
+        # The self.state variable allows the periodic function to run different conditions one after the other.
+        self.state = 0
     
         
     def autonomousPeriodic(self):
+        # time variabloe stores the time giver by the timer 
         time = self.timer.get()
-        
-        if self.intakeOn == 0:
+
+        # state 0 tells the robot to move forward for 4 seconds 
+        if self.state == 0:
             if time < 4.0:
                 self.drive.driveCartesian(.25,0,0)
             else:
                 self.drive.driveCartesian(0,0,0)
-                self.intake.set(0)
                 self.intakeOn = 1 
 
-        
-        if self.intakeOn == 1:
+        # state 1 tells the robot to turn the intake motor on 
+        if self.state == 1:
 
             self.intake.set(.5)
 
             if time >= 6.0:
                 self.intakeOn = 2 
-        
-        if self.intakeOn == 2:
-            self.drive.driveCartesian(0,0,0)
+
+        # state 2 stops the timer and turns off the intake motor 
+        # (FYI, this can be done in state 1's if statement instead of making a new state)
+        if self.state == 2:
             self.intake.set(0)
             self.timer.stop()
 
@@ -110,7 +106,8 @@ class MyRobot(wpilib.TimedRobot):
         # drive cartesian allows the robot to move 
         self.drive.driveCartesian(accelerationY, accelerationX, rotation)
         #self.drive.driveCartesian(leftY, leftX, rotation)
-        
+
+        # prints the values of leftY and leftX in the driver stattion console
         print(f"Y Speed:{leftY}" + "\n" + f"X Speed: {leftX}")
 
         # turns intake motor on when right trigger is pressed 
